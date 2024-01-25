38b5478c60af485fff165fb7b95413cc9cafb51e
Liger <gjfleo@yeah.net>
2024-01-25 15:08:51 +0800

    update

--- .vuepress/components/Color.vue
+++ .vuepress/components/Color.vue
@@ -15,12 +15,17 @@
     </div>
     <div class="color-cell font-mono" :style="{ color, backgroundColor: contrastColor }">{{ color }}</div>
     <div class="color-cell font-mono" :style="{ backgroundColor: color, color: contrastColor }">{{ color }}</div>
+    <div class="color-cell">
+      <div class="i-carbon:copy cursor-pointer hover:text-[var(--c-brand)]" @click="copy(color) " />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { Ref } from "vue";
 import { inject } from "vue";
+
+import { useClipboard } from "@vueuse/core";
 
 defineProps<{
   name: string;
@@ -31,4 +36,6 @@ const contrastColor = inject<Ref<string>>("contrastColor");
 
 const colorType = inject<string>("colorType");
 const defaultContrastColor = inject<string>("defaultContrastColor");
+
+const { copy } = useClipboard();
 </script>
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,7 @@
 import {
   defineConfig,
   presetAttributify,
+  presetIcons,
   presetUno,
 } from "unocss";
 
@@ -8,6 +9,7 @@ export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
+    presetIcons(),
   ],
   shortcuts: [
     ["color-cell", "p-block-2 flex items-center justify-center transition"],
